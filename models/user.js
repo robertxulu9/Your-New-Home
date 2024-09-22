1679ad97974ffce29fcfcc08b8576daf9b60027c
robertxulu9 <47161747+robertxulu9@users.noreply.github.com>
2024-09-22 12:10:29 +0200

    updates

--- models/user.js
+++ models/user.js
@@ -3,27 +3,28 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
 
 const User = sequelize.define('User', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  isAgent: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  registeredAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
 });
 
 module.exports = User;
